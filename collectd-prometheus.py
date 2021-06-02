1db571d26d0cce07807b579d3cf4eb6c5431b10e
Simon Lundström <simlu+github@su.se>
2021-06-02 16:06:50 +0200

    Invert verify to get the correct result.
    ssl_ignore=True means we want to ignore SSL verification which means we
    have to set verify=False

--- collectd-prometheus.py
+++ collectd-prometheus.py
@@ -94,7 +94,7 @@ class Prometheus(object):
             kwargs = {}
 
             if process.ssl_ignore is not None:
-                kwargs.update({'verify': process.ssl_ignore})
+                kwargs.update({'verify': not process.ssl_ignore})
             if process.ssl_cert is not None and process.ssl_key is not None:
                 kwargs.update({'cert': (process.ssl_cert, process.ssl_key)})
             if process.ssl_cacert is not None:
